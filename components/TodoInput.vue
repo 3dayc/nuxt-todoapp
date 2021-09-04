a0ed750e8696c02d9464651ad9a3258aacba82c6
향훈 <hyanghoon@hyanghun-ui-MacBookPro.local>
2021-09-04 17:40:59 +0900

    nuxt start

---
+++ components/TodoInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-text-field class="input-type1">
+      <v-icon slot="append" color="red"> mdi-plus </v-icon>
+    </v-text-field>
+  </div>
+</template>
+
+<style scope>
+.input-type1 {
+  width: 20rem;
+  margin: 0 auto;
+}
+</style>
---
+++ components/TodoList.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card class="mx-auto list" max-width="400" tile>
+    <ul class="list-item">
+      <li v-for="(todoItem, index) in todoItems" :key="index">
+        <span :class="todoItem.completed" @click="doneToggle(index)">{{
+          todoItem.todo
+        }}</span>
+        <span class="close-btn" @click="removeItem(index)">X</span>
+      </li>
+    </ul>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoItems: [
+        { todo: '축구', completed: '' },
+        { todo: '프리미어 리그', completed: 'done' },
+        { todo: '장보기', completed: '' },
+      ],
+    }
+  },
+  methods: {
+    doneToggle(index) {
+      if (this.todoItems[index].completed === 'done') {
+        this.todoItems[index].completed = ''
+      } else {
+        this.todoItems[index].completed = 'done'
+      }
+    },
+    removeItem(index) {
+      this.todoItems.splice(index, 1)
+    },
+  },
+}
+</script>
+
+<style scope>
+.list {
+  margin: 0 auto;
+}
+.list-item {
+  list-style: none;
+}
+.list-item > li {
+  padding: 0.5rem 1rem;
+}
+.list-item > li:first-child {
+  padding-top: 1rem;
+}
+.list-item > li:last-child {
+  padding-bottom: 1rem;
+}
+.close-btn {
+  float: right;
+}
+.done {
+  text-decoration: line-through;
+  color: gray;
+}
+</style>
